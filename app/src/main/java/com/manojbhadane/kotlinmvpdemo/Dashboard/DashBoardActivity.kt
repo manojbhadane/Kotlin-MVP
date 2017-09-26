882a5261ecba505e8ddd01b17bac3e15c4b01fc9
Manoj Bhadane <manojbhadane777@gmail.com>
2017-09-26 19:24:51 +0530

    dashboad added

---
+++ app/src/main/java/com/manojbhadane/kotlinmvpdemo/Dashboard/DashBoardActivity.kt
@@ -0,0 +1,39 @@
+package com.manojbhadane.kotlinmvpdemo.Dashboard
+
+import android.view.MenuItem
+import android.widget.TextView
+import com.manojbhadane.kotlinmvpdemo.BaseActivity
+import com.manojbhadane.kotlinmvpdemo.R
+
+/**
+ * Created by manoj.bhadane on 26-09-2017.
+ */
+class DashBoardActivity : BaseActivity() {
+
+    lateinit var mTxt: TextView
+
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_dashboard;
+    }
+
+    override fun init() {
+
+        var toolbar = findViewById(R.id.toolbar) as android.support.v7.widget.Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+
+        mTxt = findViewById(R.id.textView) as TextView
+        mTxt.text = "Manoj Bhadane"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
