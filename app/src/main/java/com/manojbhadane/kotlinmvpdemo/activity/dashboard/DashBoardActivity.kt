770bd92f1bde5a3e1170119c0e3e9056aa7ceaf8
Manoj Bhadane <manojbhadane777@gmail.com>
2017-12-28 18:45:01 +0530

    code optimised

--- app/src/main/java/com/manojbhadane/kotlinmvpdemo/activity/dashboard/DashBoardActivity.kt
+++ app/src/main/java/com/manojbhadane/kotlinmvpdemo/activity/dashboard/DashBoardActivity.kt
@@ -1,6 +1,5 @@
 package com.manojbhadane.kotlinmvpdemo.activity.dashboard
 
-import android.content.Intent
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
@@ -8,12 +7,11 @@ import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
-import com.manojbhadane.kotlinmvpdemo.activity.anagramGame.AnagramActivity
+import com.manojbhadane.kotlinmvpdemo.R
 import com.manojbhadane.kotlinmvpdemo.activity.BaseActivity
+import com.manojbhadane.kotlinmvpdemo.adapter.RecyclerViewAdapter
 import com.manojbhadane.kotlinmvpdemo.listener.RecyclerViewClickListener
 import com.manojbhadane.kotlinmvpdemo.model.MyData
-import com.manojbhadane.kotlinmvpdemo.R
-import com.manojbhadane.kotlinmvpdemo.adapter.RecyclerViewAdapter
 
 /**
  * Created by manoj.bhadane on 26-09-2017.
@@ -22,7 +20,6 @@ class DashBoardActivity : BaseActivity(), DashBoardView {
 
     lateinit var mTxt: TextView
     lateinit var mRecyclerView: RecyclerView
-
     private var presenter: DashBoardPresenterImpl? = null
 
     override fun getLayoutResId(): Int {
@@ -39,6 +36,7 @@ class DashBoardActivity : BaseActivity(), DashBoardView {
 
         mTxt = findViewById(R.id.textView) as TextView
         mRecyclerView = findViewById(R.id.recyclerview) as RecyclerView
+        mRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
 
         mTxt.text = "Recyclerview using kotlin"
 
@@ -46,11 +44,9 @@ class DashBoardActivity : BaseActivity(), DashBoardView {
     }
 
     override fun showList(list: ArrayList<MyData>) {
-        mRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
         val adapter = RecyclerViewAdapter(list, object : RecyclerViewClickListener {
             override fun onItemClick(position: Int) {
                 Toast.makeText(this@DashBoardActivity, "${position + 1} is clicked", Toast.LENGTH_SHORT).show()
-                startActivity(Intent(this@DashBoardActivity, AnagramActivity::class.java))
             }
         })
         mRecyclerView.adapter = adapter
--- app/src/main/java/com/manojbhadane/kotlinmvpdemo/activity/dashboard/DashBoardPresenter.kt
+++ app/src/main/java/com/manojbhadane/kotlinmvpdemo/activity/dashboard/DashBoardPresenter.kt
@@ -4,5 +4,7 @@ package com.manojbhadane.kotlinmvpdemo.activity.dashboard
  * Created by manoj.bhadane on 27-09-2017.
  */
 interface DashBoardPresenter {
+
     fun bindlList()
+
 }
